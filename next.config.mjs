e7f092abe1669abba37b70367fde76656bd49497
sadik-malik <malik0786sam@gmail.com>
2026-04-18 20:36:38 +0530

    feat: change homepage redirect to rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        destination: '/docs',
-        permanent: false,
-        source: '/',
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
-        destination: '/llms.mdx/docs/:path*',
+        source: '/',
+        destination: '/docs',
+      },
+      {
         source: '/docs/:path*.mdx',
+        destination: '/llms.mdx/docs/:path*',
       },
     ];
   },
